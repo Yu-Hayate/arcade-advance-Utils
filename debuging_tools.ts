5efcea1acde03ed2c36dd355e3cba064211fc0fe
Yu-Hayate <112352934+Yu-Hayate@users.noreply.github.com>
2024-11-16 13:24:42 -0400

    Update debuging_tools.ts

--- debuging_tools.ts
+++ debuging_tools.ts
@@ -2,25 +2,25 @@
 //% color="#3c1a13" icon="\uf056"
 //% advanced=true
 //% block="tools" // English
-//% block.loc.zh="工具" // Simplified Chinese
-//% block.loc.zh-tw="工具" // Traditional Chinese
+//% block.loc.zh-cn="工具" // Chinese
+//% block.loc.zh-tw="工具" //  Chinese
 //% block.loc.de="Werkzeuge" // German
 //% block.loc.fr="outils" // French
 //% block.loc.ja="ツール" // Japanese
-//% block.loc.es="herramientas" // Spanish (Spain)
+//% block.loc.es-es="herramientas" // Spanish (Spain)
 //% block.loc.es-mx="herramientas" // Spanish (Mexico)
 //% block.loc.ru="инструменты" // Russian
 namespace tools
 {
     //% blockId=throw_error
     //% block="throw error $error" // English
-    //% block.loc.zh-cn="抛出错误 $error" // Simplified Chinese
-    //% block.loc.zh-tw="拋出錯誤 $error" // Traditional Chinese
+    //% block.loc.zh-cn="抛出错误 $error" // Chinese
+    //% block.loc.zh-tw="拋出錯誤 $error" // Chinese
     //% block.loc.de="Fehler werfen $error" // German
     //% block.loc.fr="lancer une erreur $error" // French
     //% block.loc.ja="エラーを投げる $error" // Japanese
-    //% block.loc.es-es="lanzar error $error" // Spanish (Spain)
-    //% block.loc.es-mx="lanzar error $error" // Spanish (Mexico)
+    //% block.loc.es-es="lanzar error $error" // Spanish 
+    //% block.loc.es-mx="lanzar error $error" // Spanish 
     //% block.loc.ru="выдать ошибку $error" // Russian
     //% error.shadow="text"
     export function throwError(error: any) {
